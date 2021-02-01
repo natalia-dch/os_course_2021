a54c02e662071d63c4d94bc2d5aa2c4ce3572991
Доскач Наталья <nadoskach@edu.hse.ru>
2021-02-01 15:18:43 +0300

    task1

--- 1.c
+++ 1.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 #include <stdio.h>
 int main(int args,char **argv){
-printf("UID=%d\nGID=%d\n",getuid(),getgid());
+printf("PPID=%d\nPID=%d\n",getppid(),getpid());
 return 0;
 
 }
