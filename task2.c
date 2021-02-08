32a725f778ef58cdf7b9c786b3154640025be8df
Доскач Наталья <nadoskach@edu.hse.ru>
2021-02-08 15:49:02 +0300

    task2

--- task2.c
+++ task2.c
@@ -2,12 +2,12 @@
 #include <unistd.h>
 #include <stdio.h>
 
-int main(){
+int main(int argc, char *argv[], char *envp[]){
 pid_t  pid = fork();
 if (pid == -1)
 	printf("Error happened");
 else if (pid == 0) 
-        printf("It's a kid. PID = %d",getpid());
+        execle("/bin/cat","/bin/cat", "task1.c",0, envp);
 else
 	printf("It's a parent. PID = %d",getpid());
 return 0;
