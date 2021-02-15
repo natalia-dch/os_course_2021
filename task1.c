65a1c618335fb69d7ae1c91167d1713c463315f2
Доскач Наталья <nadoskach@edu.hse.ru>
2021-02-15 16:08:02 +0300

    t1 added

--- task1.c
+++ task1.c
@@ -1,17 +1,30 @@
 #include <sys/types.h>
-#include <unistd.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
-int main(int argc, char *argv[], char *envp[])
+int main()
 {
-for(int i=0;i<argc;i++){
-printf("%s\n",argv[i]);
-}
-int i=0;
-while(envp[i]!=0){
-printf("%s\n",envp[i]);
-i++;
-}
-return 0;
+  int     fd;
+  size_t  size;
+  char    string[14];
+  (void)umask(0);
+ 
+  if ((fd = open("myfile", O_RDONLY, 0666)) < 0) {
+    printf("Can\'t open file\n");
+    exit(-1);
+  }
+  //
+  // Try to write 14 bytes from our array to the file,
+  // i.e. the entire string "Hello, world!" along with the end-of-line sign.
+  //
+  read(fd, string, 14);
+
+  if (close(fd) < 0) {
+    printf("Can\'t close file\n");
+  }
+  printf(string);
+  return 0;
 }
