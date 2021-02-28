d91baea267c39b7bc78a0643ff88a176e40d938a
Доскач Наталья <nadoskach@edu.hse.ru>
2021-02-28 16:12:12 +0300

    tasks done

--- task1.c
+++ task1.c
@@ -4,27 +4,9 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <unistd.h>
-
+#include <limits.h>
 int main()
 {
-  int     fd;
-  size_t  size;
-  char    string[14];
-  (void)umask(0);
- 
-  if ((fd = open("myfile", O_RDONLY, 0666)) < 0) {
-    printf("Can\'t open file\n");
-    exit(-1);
-  }
-  //
-  // Try to write 14 bytes from our array to the file,
-  // i.e. the entire string "Hello, world!" along with the end-of-line sign.
-  //
-  read(fd, string, 14);
-
-  if (close(fd) < 0) {
-    printf("Can\'t close file\n");
-  }
-  printf(string);
+  printf("Pipe buffer size:%d",PIPE_BUF);
   return 0;
 }
