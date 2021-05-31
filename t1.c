f5fe74c8f7885f6c90722ffe46038e2f95ab903d
Доскач Наталья <nadoskach@edu.hse.ru>
2021-05-31 15:59:54 +0300

    tasks done

--- t1.c
+++ t1.c
@@ -1,33 +1,14 @@
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdio.h>
+#include <signal.h>
 
-
-int main(void)
-{
-  char buffer1 [50];
-  char buffer2 [50];
-  char *name ="a0.txt";
-  int n = 0;
-  int c1;
-  int fd;
-  if((fd = open(name,O_CREAT | O_WRONLY,0666)) == -1){
-	  printf("cannot create file");}
-	  
-  while(n<100){
-	c1 = sprintf(buffer1,"a%d.txt",n);
-	close(fd);
-	n++; 
-	c1 = sprintf(buffer2,"a%d.txt",n);
-    fd = symlink(buffer1,buffer2);
-    if((fd = open(buffer2,O_WRONLY,0666)) == -1){
-		n--;
-		printf("Не могу открыть %s. Глубина рекурсии: %d",buffer2,n);
-		close(fd);
-		return 0;
-		}
-	  }
-
-
+int main(void) {
+  //
+  // Set the process response to the SIGINT signal to ignore
+  //
+  (void)signal(SIGQUIT, SIG_IGN);
+  (void)signal(SIGINT, SIG_IGN);
+  //
+  // From this point, the process will ignore the occurrence of the SIGINT signal.
+  //
+  while(1);
   return 0;
 }
